044606dfec62f09368f82dc6fb2c20169f0ce26e
ADMIN1 <ADMIN1@192.168.0.9>
2018-01-14 01:44:45 +0530

    first commit

---
+++ Camnunda-demo1/src/main/java/com/maoj/training/camunda/demo1/LoanApprovalApplication.java
@@ -0,0 +1,9 @@
+package com.maoj.training.camunda.demo1;
+
+import org.camunda.bpm.application.ProcessApplication;
+import org.camunda.bpm.application.impl.ServletProcessApplication;
+
+@ProcessApplication("Loan Approval App")
+public class LoanApprovalApplication extends ServletProcessApplication {
+
+}
